64d301a15a8cb5f95820e56fd3b83513875051e5
Adrian Unruh <unruha@uw.edu>
2021-03-19 21:28:54 -0700

    added random roll button

--- src/components/RandomRoll.vue
+++ src/components/RandomRoll.vue
@@ -19,11 +19,14 @@
                 <input type="number" id="num5" name="num5" min="1" max="5" disabled>
             </label>
         </div>
-        <div>
+        <div class="scoreDiv">
             <label class="inputLabel">
                 <b>Score: </b>
                 <input class="score" type="number" id="score" name="score" disabled>
             </label>
+        </div>
+        <div class="buttonDiv">
+            <button class="randomButton">Get Random!</button>
         </div>
     </div>
 </template>
@@ -45,5 +48,30 @@
 }
 .score {
     width: 10%;
+}
+.randomButton {
+    display: inline-block;
+    padding: 15px 25px;
+    font-size: 18px;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    outline: none;
+    color: #fff;
+    background-color: #1E90FF;
+    border: none;
+    border-radius: 10px;
+    box-shadow: 0 4px #999;
+}
+.randomButton:active {
+    background-color: #3e8e41;
+    box-shadow: 0 4px #666;
+    transform: translateY(2px);
+}
+.buttonDiv {
+    height: 200px;
+}
+.scoreDiv {
+    height: 100px;
 }
 </style>
